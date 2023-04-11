80c8453fc659f50c12f5c49eee190b64254dd52f
jhkim <jinhankim988@gmail.com>
2023-04-11 19:41:46 +0900

    [Fix] 오타 수정

--- dzauth/src/main/java/com/douzone/dzauth/dto/UserDTO.java
+++ dzauth/src/main/java/com/douzone/dzauth/dto/UserDTO.java
@@ -14,7 +14,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class xUserDTO {
+public class UserDTO {
     @NotBlank
     private String username;
     @NotBlank
